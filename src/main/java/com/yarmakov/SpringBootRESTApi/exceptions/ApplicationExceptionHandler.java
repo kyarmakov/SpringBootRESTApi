a4405ea6d29437f4ab5afe8291d24825dbbb821a
Kerim Yarmakov <kyarmakov@gmail.com>
2024-03-28 22:50:54 +0300

    Add UserNotFoundException handler

--- src/main/java/com/yarmakov/SpringBootRESTApi/exceptions/ApplicationExceptionHandler.java
+++ src/main/java/com/yarmakov/SpringBootRESTApi/exceptions/ApplicationExceptionHandler.java
@@ -1,8 +1,16 @@
 package com.yarmakov.SpringBootRESTApi.exceptions;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
 public class ApplicationExceptionHandler {
 
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    @ExceptionHandler(UserNotFoundException.class)
+    public String handleUserNotFound(Exception e) {
+        return e.getMessage();
+    }
 }
